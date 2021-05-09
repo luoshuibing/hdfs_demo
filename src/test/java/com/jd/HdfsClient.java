72e61cf32fb21ca232e43ae76fd8d717cef33699
luoshuibing <luoshuibing@126.com>
2021-05-09 11:02:26 +0800

    初始化Hadoop_Hdfs

---
+++ src/test/java/com/jd/HdfsClient.java
@@ -0,0 +1,171 @@
+package com.jd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+/**
+ * @author FM
+ * @Description
+ * @create 2021-05-08 23:35
+ */
+@Slf4j
+public class HdfsClient {
+
+    /**
+     * 添加VM参数
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testMkdirs1() throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", "hdfs://hadoop144:9000");
+        FileSystem fs = FileSystem.get(configuration);
+        fs.mkdirs(new Path("/2021/hadoop1"));
+        fs.close();
+    }
+
+    @Test
+    public void testMkdirs2() throws Exception {
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+        fs.mkdirs(new Path("/2021/hadoop3"));
+        fs.close();
+    }
+
+    /**
+     * 参数优先级排序： （1）客户端代码中设置的值 >（2）classpath下的用户自定义配置文件 >（3）然后是服务器的默认配置
+     * @throws Exception
+     */
+    @Test
+    public void testCopyFromLocalFile() throws Exception {
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        configuration.set("dfs.replication", "2");
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+
+        // 2 上传文件
+        fs.copyFromLocalFile(new Path("e:/hello.txt"), new Path("/hello.txt"));
+
+        // 3 关闭资源
+        fs.close();
+
+        System.out.println("over");
+    }
+
+    @Test
+    public void testCopyToLocalFile() throws IOException, InterruptedException, URISyntaxException{
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+
+        // 2 执行下载操作
+        // boolean delSrc 指是否将原文件删除
+        // Path src 指要下载的文件路径
+        // Path dst 指将文件下载到的路径
+        // boolean useRawLocalFileSystem 是否开启文件校验
+        fs.copyToLocalFile(false, new Path("/hello.txt"), new Path("e:/hello.txt"), true);
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testDelete() throws IOException, InterruptedException, URISyntaxException{
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+
+        // 2 执行删除
+        fs.delete(new Path("/hello.txt"), true);
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testRename() throws IOException, InterruptedException, URISyntaxException{
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+
+        // 2 修改文件名称
+        fs.rename(new Path("/hello.txt"), new Path("/hello6.txt"));
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testListFiles() throws IOException, InterruptedException, URISyntaxException{
+        // 1获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+
+        // 2 获取文件详情
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+
+        while(listFiles.hasNext()){
+            LocatedFileStatus status = listFiles.next();
+
+            // 输出详情
+            // 文件名称
+            // System.out.println(status.getPath());
+            System.out.println(status.getPath().getName());
+            // 长度
+            System.out.println(status.getLen());
+            // 权限
+            System.out.println(status.getPermission());
+            // z组
+            System.out.println(status.getGroup());
+
+            // 获取存储的块信息
+            BlockLocation[] blockLocations = status.getBlockLocations();
+
+            for (BlockLocation blockLocation : blockLocations) {
+
+                // 获取块存储的主机节点
+                String[] hosts = blockLocation.getHosts();
+
+                for (String host : hosts) {
+                    System.out.println(host);
+                }
+            }
+
+            System.out.println("----------------班长的分割线-----------");
+        }
+    }
+
+    @Test
+    public void testListStatus() throws IOException, InterruptedException, URISyntaxException{
+
+        // 1 获取文件配置信息
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop144:9000"), configuration, "hadoop");
+
+        // 2 判断是文件还是文件夹
+        FileStatus[] listStatus = fs.listStatus(new Path("/"));
+
+        for (FileStatus fileStatus : listStatus) {
+
+            // 如果是文件
+            if (fileStatus.isFile()) {
+                System.out.println("f:"+fileStatus.getPath().getName());
+            }else {
+                System.out.println("d:"+fileStatus.getPath().getName());
+            }
+        }
+
+        // 3 关闭资源
+        fs.close();
+
+    }
+
+}
